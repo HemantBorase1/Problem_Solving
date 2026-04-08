01368de4c3a78ffbc14bd3be33aefb9829133043
Hemant Borase <hborase004@gmail.com>
2026-04-08 17:31:20 +0530

    Day-2

---
+++ NumPro2.java
@@ -0,0 +1,16 @@
+class NumPro2
+{
+    public static void main(String[] args)
+    {
+           int n=1234;
+           int reverse=0;
+   
+          while(n!=0)
+          {
+             int digit =n%10;
+             reverse = reverse * 10 + digit;
+             n/=10;
+          }
+      System.out.println(reverse);
+    }
+}
