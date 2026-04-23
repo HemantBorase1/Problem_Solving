907e8dca050e6744e77c79d2516f8c4d878e237d
Hemant Borase <hborase004@gmail.com>
2026-04-23 19:11:14 +0530

    Day-9

---
+++ NumPro9.java
@@ -0,0 +1,14 @@
+class NumPro9
+{
+
+     public static void main(String[] args)
+     {
+        int n=2,power=5;
+        int ans=1;
+        for(int i=0;i<power;i++)
+        {
+             ans*=n;
+        }
+       System.out.println(ans);
+     }
+}
