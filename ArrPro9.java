da190d0189cab2ff86a15369081f4cb9a4eb095a
Hemant Borase <hborase004@gmail.com>
2026-04-23 20:28:41 +0530

    Day-9

---
+++ ArrPro9.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class ArrPro9
+{
+
+    public static void main(String[] args)
+    {
+          int a[]={1,3,6};
+          int b[]={2,4,5};
+          int merge[]=new int[a.length+b.length];
+       /*   
+          for(int i=0;i<a.length;i++)
+              merge[i]=a[i];
+          System.out.println(Arrays.toString(merge));
+       */
+          for(int i=0;i<a.length;i++)
+          {
+            for(int j=0;j<a.length;j++)
+            {
+                 // Remaining the solution
+                   
+            }
+          }
+         System.out.println(Arrays.toString(merge));
+    }
+}
