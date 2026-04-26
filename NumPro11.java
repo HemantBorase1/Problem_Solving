0094ab986dd02ea99045444c5d57cf6ace4c8812
Hemant Borase <hborase004@gmail.com>
2026-04-26 19:37:35 +0530

    Day-11

--- NumPro11.java
+++ NumPro11.java
@@ -3,10 +3,10 @@ class NumPro11
 
    public static void main(String[] args)
    {
-                int n=121;
+                int n=123;
                 int temp=n;
                 int ans=0;
-            while(n>=0)
+            while(n!=0)
             {
              int digit =n%10;
              ans=ans*10+digit;
