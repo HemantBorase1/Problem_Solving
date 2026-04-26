cc6f623a8fb7906a2ddeb98c5d76e00d64597971
Hemant Borase <hborase004@gmail.com>
2026-04-26 19:35:47 +0530

    Day-11

---
+++ NumPro11.java
@@ -0,0 +1,20 @@
+class NumPro11
+{
+
+   public static void main(String[] args)
+   {
+                int n=121;
+                int temp=n;
+                int ans=0;
+            while(n>=0)
+            {
+             int digit =n%10;
+             ans=ans*10+digit;
+             n/=10;
+            }
+          if(ans==temp)
+          {
+             System.out.println("Number is Palindrome.");
+          }
+   }
+}
