e99322f58f361340b21b9e13b6bbfc43cf16cac2
Hemant Borase <hborase004@gmail.com>
2026-05-10 08:52:53 +0530

    Day-13

---
+++ ArrPro13.java
@@ -0,0 +1,16 @@
+class ArrPro13
+{
+
+    public static void main(String[] args)
+    {
+       int arr[]={1,2,3,4,5};
+       int sum=0;
+       for(int i=0;i<arr.length;i++)
+       {
+           sum+=arr[i];
+            
+       }
+       int avg=sum/arr.length;
+       System.out.println(avg);
+    }
+}
