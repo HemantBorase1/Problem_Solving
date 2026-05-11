b4ee4e229098d7a459e0b053060362211e2db38e
Hemant Borase <hborase004@gmail.com>
2026-05-11 07:23:35 +0530

    Day-14

---
+++ StrPro14.java
@@ -0,0 +1,19 @@
+class StrPro14
+{
+
+   public static void main(String[] args)
+   {
+         int count=0;
+         String str="Hello World";
+        for(int i=0;i<str.length();i++)
+        {
+               if(str.charAt(i)==" ")
+               {
+
+                } 
+              else
+                count++
+         }
+        System.out.println(count);
+   }
+}
